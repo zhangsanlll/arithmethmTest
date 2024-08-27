794bfbdd8a0363aff9ab270908ce8b7126757d7f
zhangsanlll <18481017932@163.com>
2024-08-28 00:19:15 +0800

    滑动窗口

---
+++ sliding_window/src/LeetCode01_209.java
@@ -0,0 +1,32 @@
+
+/*
+209.长度最小的子数组
+给定一个含有 n 个正整数的数组和一个正整数 target 。
+
+找出该数组中满足其总和大于等于 target 的长度最小的
+子数组
+ [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+* */
+public class LeetCode01_209 {
+    public static int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        //这里min不能是-1，因为后面要求最小值，-1会干扰结果
+        int min = Integer.MAX_VALUE;
+        while (right < nums.length && left < nums.length) {
+            sum += nums[right];
+            while (sum >= target) {
+                // 满足结果，更新最小值
+                int len = right - left + 1;
+                min = Math.min(len, min);
+                sum = sum - nums[left++];
+                //left++;
+            }
+            right++;
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+
+
+    }
+}
---
+++ sliding_window/src/LeetCode02_3.java
@@ -0,0 +1,36 @@
+
+/*给定一个字符串 s ，请你找出其中不含有重复字符的 最长
+子串
+ 的长度。
+* */
+public class LeetCode02_3 {
+    public  static int lengthOfLongestSubstring(String ss) {
+        //使用滑动窗口＋哈希表
+        //如果哈希表能用数组代替尽量用数组代替，因为数组更快
+        //将字符串转变成字符数组
+        char[] s = ss.toCharArray();
+        int[] hash = new int[128];
+        int len = 0;
+        //有效字符：哈希表中字符的个数为1
+        for(int left = 0,right = 0;right < ss.length();right++){
+            //进窗口，让hash进入窗口
+            hash[s[right]]++;
+            //判断,窗口内出现重复字符
+            while(hash[s[right]] > 1){
+                //滑动窗口中已经有了该有效字符
+                //出窗口，从哈希表中删除该字符
+                //hash[s[right]]--;
+                hash[s[left++]]--;
+            }
+            //更新结果
+            len =Math.max(len,right-left+1);
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        String str = "pwwkew";
+        int ret = lengthOfLongestSubstring(str);
+        System.out.println(ret);
+    }
+}
