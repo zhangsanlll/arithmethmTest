e6869590ac35a7f24d679db413e2ec0de5fe51d2
zhangsanlll <18481017932@163.com>
2024-08-29 00:25:51 +0800

    滑动窗口

--- sliding_window/src/com/main/LeetCode01_209.java
+++ sliding_window/src/com/main/LeetCode01_209.java
@@ -1,11 +1,9 @@
+package com.main;
 
 /*
 209.长度最小的子数组
-给定一个含有 n 个正整数的数组和一个正整数 target 。
-
-找出该数组中满足其总和大于等于 target 的长度最小的
-子数组
- [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+给定一个含有 n 个正整数的数组和一个正整数 target 。找出该数组中满足其总和大于等于 target 的长度最小的
+子数组[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
 * */
 public class LeetCode01_209 {
     public static int minSubArrayLen(int target, int[] nums) {
--- sliding_window/src/com/main/LeetCode02_3.java
+++ sliding_window/src/com/main/LeetCode02_3.java
@@ -1,7 +1,7 @@
+package com.main;
 
-/*给定一个字符串 s ，请你找出其中不含有重复字符的 最长
-子串
- 的长度。
+/*
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串的长度。
 * */
 public class LeetCode02_3 {
     public  static int lengthOfLongestSubstring(String ss) {
